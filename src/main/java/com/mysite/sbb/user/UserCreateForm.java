522b19db0faec0beb59f71992bf23bc548cb8430
kimmj5909 <kimmj5909@kfca.re.kr>
2025-10-31 11:58:03 +0900

    20251031

--- src/main/java/com/mysite/sbb/user/UserCreateForm.java
+++ src/main/java/com/mysite/sbb/user/UserCreateForm.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,8 +28,9 @@ public class UserCreateForm {
 	@Email
 	private String email;
 	
-	//개선사항 
+	// 연락처는 숫자만으로 10~11자리 입력을 강제한다.
 	@NotEmpty(message = "연락처는 필수 항목입니다.")
-	@Size(min = 10, max = 11) 
+	@Size(min = 10, max = 11)
+	@Pattern(regexp = "\\d{10,11}", message = "연락처는 숫자만 10~11자리로 입력하세요.")
 	private String phone;
 }
